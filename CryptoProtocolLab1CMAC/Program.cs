4dc1d7d33b03b0ca57255c4313678e4798e51874
PunkyIANG <tsoorckanukris@gmail.com>
2021-09-26 21:22:16 +0300

    finished hmac, started cmac

---
+++ CryptoProtocolLab1CMAC/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace CryptoProtocolLab1CMAC
+{
+    class Program
+    {
+        static byte[] zero = new byte[16];
+        private static byte[] rb = new byte[16];
+
+        static void Main(string[] args)
+        {
+            rb[0] = 0x87;
+            
+            
+            Console.WriteLine("Hello World!");
+        }
+
+        public void GenerateSubkey(byte[] k, out byte[] k1, out byte[] k2)
+        {
+            var aes = Aes.Create();
+            byte[] L;
+            k1 = new byte[16];
+            k2 = new byte[16];
+
+            aes.Key = k;
+            
+            var encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+            
+            using (MemoryStream msEncrypt = new MemoryStream())
+            {
+                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                    {
+                        //Write all data to the stream.
+                        swEncrypt.Write(zero);
+                    }
+                    L = msEncrypt.ToArray();
+                }
+            }
+            
+            //bit shift over whole key
+            var temp = new BitArray(L);
+            for (int i = 0; i < temp.Length - 1; i++)
+                temp[i] = temp[i + 1];
+            
+            temp.CopyTo(k1, 0);
+
+            //if most significant bit is zero
+            if ((L[^1] & 128) == 0)
+            {
+                for (int i = 0; i < k1.Length; i++)
+                {
+                    k1[i] ^= rb[i];
+                }
+            }
+            
+            //bit shift over whole key
+            var temp2 = new BitArray(k1);
+            for (int i = 0; i < temp2.Length - 1; i++)
+                temp2[i] = temp2[i + 1];
+            
+            temp2.CopyTo(k2, 0);
+
+            //if most significant bit is zero
+            if ((L[^1] & 128) == 0)
+            {
+                for (int i = 0; i < k2.Length; i++)
+                {
+                    k2[i] ^= rb[i];
+                }
+            }
+        }
+    }
+}
---
+++ CryptoProtocolLab1HMAC/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CryptoProtocolLab1HMAC
+{
+    class Program
+    {
+        const int k0Length = 64;
+        const int hashLength = 32;
+        const byte ipad = 0x36;
+        const byte opad = 0x5c;
+
+        static void PrintByteArr(byte[] arr)
+        {
+            string hex = BitConverter.ToString(arr);
+            Console.WriteLine(hex);
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                byte[] k    = Encoding.ASCII.GetBytes("key");
+                byte[] text = Encoding.ASCII.GetBytes("The quick brown fox jumps over the lazy dog");
+
+                HMACSHA256 hmac = new HMACSHA256(k);
+                
+                Console.WriteLine("hmac 'key' 'text'");
+                Console.WriteLine("Example: ");
+                Console.WriteLine("Key: key");
+                Console.WriteLine("Text: The quick brown fox jumps over the lazy dog");
+                Console.Write("HMAC value:     ");
+                PrintByteArr(HMACStuff(k, text));
+
+                Console.Write("Standard value: ");
+                PrintByteArr(hmac.ComputeHash(text));
+            }
+            else if (args.Length == 1)
+            {
+                Console.WriteLine("This program requires 2 parameters to run");
+            }
+            else
+            {
+                byte[] k    = Encoding.ASCII.GetBytes(args[0]);
+                byte[] text = Encoding.ASCII.GetBytes(args[1]);
+
+                HMACSHA256 hmac = new HMACSHA256(k);
+
+                Console.Write("HMAC value:     ");
+                PrintByteArr(HMACStuff(k, text));
+
+                Console.Write("Standard value: ");
+                PrintByteArr(hmac.ComputeHash(text));
+            }
+        }
+
+        static byte[] HMACStuff(byte[] k, byte[] text)
+        {
+            HashAlgorithm sha = SHA256.Create();
+            
+            byte[] k0;
+            if (k.Length == k0Length)
+            {
+                k0 = k;
+            } 
+            else if (k.Length > k0Length)
+            {
+                k0 = new byte[k0Length];
+                Array.Copy(sha.ComputeHash(k), k0, hashLength);
+            } 
+            else
+            {
+                k0 = new byte[k0Length];
+                Array.Copy(k, k0, k.Length);
+            }
+            
+            byte[] IKeyPad = new byte[k0Length + text.Length];
+
+            for (int i = 0; i < k0Length; i++)
+                IKeyPad[i] = (byte)(k0[i] ^ ipad);
+
+            Array.Copy(text, 0, IKeyPad, k0Length, text.Length);
+
+            var V = sha.ComputeHash(IKeyPad);
+            
+            byte[] OKeyPad = new byte[k0Length + hashLength];
+
+            for (int i = 0; i < k0Length; i++)
+                OKeyPad[i] = (byte)(k0[i] ^ opad);
+
+            Array.Copy(V, 0, OKeyPad, k0Length, hashLength);
+            
+            return sha.ComputeHash(OKeyPad);
+        }
+    }
+}
